c647bcc125a569a0f85b3807702dcf4fed15ee9d
Steve Avsec <stephen.avsec@opendatagroup.com>
2020-05-18 15:41:18 -0500

    Initial model, external file, and test data

---
+++ test_model.py
@@ -0,0 +1,29 @@
+import json
+import math
+
+#modelop.init
+def begin():
+    global coefs
+    coefs = json.load(open('external_file_asset.json', 'r'))
+    pass
+
+#modelop.score
+def action(datum):
+    prediction = compute_prediction(datum)
+    yield prediction
+
+def compute_prediction(datum):
+    x_score = coefs['x']*datum['x'] 
+    y_score = coefs['y']*datum['y'] 
+    prediction = x_score + y_score + coefs['intercept']
+    return prediction
+
+#modelop.metrics
+def metrics(data):
+    actuals = data.z.tolist()
+    data = data.to_dict(orient='records')
+    predictions = list(map(compute_prediction, data))
+    diffs = [x[0] - x[1] for x in zip(actuals, predictions)]
+    rmse = math.sqrt(sum(list(map(lambda x: x**2, diffs)))/len(diffs))
+    mae = sum(list(map(abs, diffs)))/len(diffs)
+    yield dict(MAE=mae, RMSE=rmse)
